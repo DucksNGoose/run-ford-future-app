ec921e3af1c4df10f66948eac222c832b2df6b09
Henrique Alves <henri.falves@hotmail.com>
2019-09-28 00:23:11 -0300

    [BIG_CHANGE/A_NEW_BEGINNING] remove the old api, to add new ruby on rails rest api

--- image-web-app/busi_api/lib/busi_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule BusiApi.Repo do
-  use Ecto.Repo,
-    otp_app: :busi_api,
-    adapter: Ecto.Adapters.Postgres
-end
--- image-web-app/busi_api/lib/busi_api_web/controllers/default_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule BusiApiWeb.DefaultController do
-  use BusiApiWeb, :controller
-
-  def index(conn, _params) do
-    text conn, "BusiApi!"
-  end
-end
