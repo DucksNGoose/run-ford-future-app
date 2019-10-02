c6033eeaef01dc466b641be4d4b70577219fd486
henrique.alves <henri.falves@hotmail.com>
2019-10-02 02:29:12 -0300

    [rick] - add image post route in Image Controller

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -4,7 +4,21 @@ module Api
            def index
                 images = Image.order('created_at DESC');
                 render json: {status: 'SUCCESS', message:'Images already loaded', data:images},status: :ok 
-           end  
+           end
+           
+           def create
+                image = Image.new(image_params)
+
+                if image.save
+                    render json: {status: 'SUCCESS', message:'Saved article', data:image},status: :ok
+                else
+                    render json: {status: 'ERROR', message:'Image not saved', data:image.erros},status: :unprocessable_entity
+                end
+            end
+            
+            def image_params
+                params.permit(:img_name, :base64_img)
+            end
         end
     end
 end
